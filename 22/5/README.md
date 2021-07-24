7e122e26fe5d344ec2f5ad5f1b19ff055a190169
cody1991 <codytang@tencent.com>
2021-07-24 13:49:35 +0800

    go func,method

---
+++ 22/5/README.md
@@ -0,0 +1,5 @@
+```go
+func funcName(params) result {
+  body
+}
+```
---
+++ 22/5/method.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// 方法必须有一个接收者，接收者是一个类型
+// 方法和类型绑定在一起，称为类型的方法
+type Age uint
+
+// age Age 是接收者
+// age 是变量名，Age是接收者类型
+func (age Age) String() {
+	fmt.Println("the age is ", age)
+}
+
+// 指针类型可以修改值
+func (age *Age) Modify(value Age) {
+	*age = Age(value)
+}
+
+func main() {
+	age := Age(1)
+	age.String()
+
+	age.Modify(20)
+	age.String()
+}
