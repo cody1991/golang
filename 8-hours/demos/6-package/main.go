0e9e7efcca8f9828519449d11e7ed52d21e6e82e
cody1991 <codytang@tencent.com>
2021-08-28 15:41:31 +0800

    import 学习

--- 8-hours/demos/6-package/main.go
+++ 8-hours/demos/6-package/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"golang/8-hours/demos/6-package/lib1"
-	"golang/8-hours/demos/6-package/lib2"
+	myLib1 "golang/8-hours/demos/6-package/lib1" // 别名
+	_ "golang/8-hours/demos/6-package/lib2"      // 匿名 _ ，导入不使用，会调用包的 init 方法
+	// . "golang/8-hours/demos/6-package/lib2"  // 直接导入整个包，可以使用里面的方法。可能会有重复的冲突
 )
 
 func main() {
 	fmt.Println("main")
 
-	lib1.Api()
-	lib2.Api()
+	myLib1.Api()
+	// lib2.Api()
+	// Api()
 }
